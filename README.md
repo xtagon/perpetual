ace1fb6cb41dd4536da0d883e504864323f563d8
Justin Workman <xtagon@gmail.com>
2020-10-25 17:23:32 -0700

    Pre-release cleanup, adding development tasks.

--- README.md
+++ README.md
@@ -8,11 +8,18 @@ different points in time, and in addition to that, `Perpetual` lets you
 define a function for repeatedly updating the stored state for as long as the
 process is kept running.
 
+## Status
+
+This is an early work in progress, and should be considered experimental,
+incomplete, and unstable until v1.0.0, following [Semantic Versioning][semver].
+
+All notable changes will be recorded in the [changelog](CHANGELOG.md).
+
 ## Installation
 
-Once [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `perpetual` to your list of dependencies in `mix.exs`. For now, you
-can test the development branch with:
+Once available in Hex, the package can be installed by adding `perpetual` to
+your list of dependencies in `mix.exs`. For now, you can test the development
+branch with:
 
 ```elixir
 def deps do
@@ -22,8 +29,23 @@ def deps do
 end
 ```
 
+## Documentation
+
+Documentation can be generated from the source code using `mix docs`.
+
+## Development
+
+The following Mix tasks are available to assist in development:
+
+- `mix docs`
+- `mix test`
+- `mix coveralls`
+- `mix credo`
+
 ## License
 
 Perpetual's source code is released under Apache License 2.0.
 
 Check [NOTICE](NOTICE) and [LICENSE](LICENSE) files for more information.
+
+[semver]: https://semver.org/
--- mix.exs
+++ mix.exs
@@ -5,9 +5,34 @@ defmodule Perpetual.MixProject do
     [
       app: :perpetual,
       version: "0.0.1",
+      description: "A simple abstraction around repeatedly iterating state in Elixir.",
+      package: package(),
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Perpetual",
+      source_url: "https://github.com/xtagon/perpetual",
+      homepage_url: "https://github.com/xtagon/perpetual",
+      docs: [
+        main: "readme",
+        extras: [
+          "README.md",
+          "CHANGELOG.md",
+          "LICENSE",
+          "NOTICE"
+        ]
+      ],
+
+      # Coverage
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -21,8 +46,29 @@ defmodule Perpetual.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.13", only: :test}
+    ]
+  end
+
+  def package do
+    [
+      # These are the default files included in the package
+      files: ~w(
+        .credo.exs
+        .formatter.exs
+        README.md
+        CHANGELOG.md
+        LICENSE
+        NOTICE
+        lib
+        mix.exs
+      ),
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/xtagon/perpetual"
+      }
     ]
   end
 end
